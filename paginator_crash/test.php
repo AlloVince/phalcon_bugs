7596482936045e8b2cdd9483128aef550cca9120
AlloVince <allo.vince@gmail.com>
2014-04-10 13:31:34 +0800

    add

--- paginator_crash/test.php
+++ paginator_crash/test.php
@@ -8,7 +8,7 @@ $di->set('db', function(){
     return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
         "host" => "localhost",
         "username" => "root",
-        "password" => "582tsost",
+        "password" => "",
         "dbname" => "scrapy"
     ));
 });
