cc85ab44999ad93ebeffed555871d02643e20cb1
AlloVince <allo.vince@gmail.com>
2014-06-23 23:21:42 +0800

    test project

---
+++ throw_exception_get_blank/eva/apps/frontend/Module.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Eva\Frontend;
+
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+use Phalcon\Mvc\ModuleDefinitionInterface;
+
+
+class Module implements ModuleDefinitionInterface
+{
+
+    /**
+     * Registers the module auto-loader
+     */
+    public function registerAutoloaders()
+    {
+
+        $loader = new Loader();
+
+        $loader->registerNamespaces(array(
+            'Eva\Frontend\Controllers' => __DIR__ . '/controllers/',
+            'Eva\Frontend\Models' => __DIR__ . '/models/',
+        ));
+
+        $loader->register();
+    }
+
+    /**
+     * Registers the module-only services
+     *
+     * @param Phalcon\DI $di
+     */
+    public function registerServices($di)
+    {
+
+        /**
+         * Read configuration
+         */
+        $config = include __DIR__ . "/config/config.php";
+
+        /**
+         * Setting up the view component
+         */
+        $di['view'] = function () {
+            $view = new View();
+            $view->setViewsDir(__DIR__ . '/views/');
+
+            return $view;
+        };
+
+        /**
+         * Database connection is created based in the parameters defined in the configuration file
+         */
+        $di['db'] = function () use ($config) {
+            return new DbAdapter(array(
+                "host" => $config->database->host,
+                "username" => $config->database->username,
+                "password" => $config->database->password,
+                "dbname" => $config->database->name
+            ));
+        };
+
+        $di->set('dispatcher', function () use ($di) {
+            $dispatcher = new \Phalcon\Mvc\Dispatcher();
+            $dispatcher->setEventsManager($di->getEventsManager());
+            return $dispatcher;
+        }, true);
+
+        $di->getEventsManager()->attach('dispatch:beforeExecuteRoute', function($dispatcher) {
+            throw new \Exception('test');
+        });
+    }
+
+}
